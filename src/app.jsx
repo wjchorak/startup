5d701594834619f9e194d9eab1861c157dfb27eb
wjchorak <westonjchorak@gmail.com>
2025-10-06 10:34:44 -0600

    Expanded app.jsx

--- src/app.jsx
+++ src/app.jsx
@@ -3,5 +3,22 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import './app.css';
 
 export default function App() {
-    return <div className='body'>App display here</div>;
+    return (
+        <div className="body">
+            <header>
+                <nav class="navbar fixed-top navbar-dark">
+                    <h1 class="navbar-brand" href="#">Blackjack</h1>
+                </nav>
+            </header>
+
+            <main>App components go here</main>
+
+            <footer>
+                <br />
+                <span>A site by Weston Chorak</span>
+                <br />
+                <a href="https://github.com/wjchorak/startup">Github</a>
+            </footer>
+        </div>
+    );
 }
