c02b277b53eacf8458799de479795bca0759e196
wjchorak <westonjchorak@gmail.com>
2025-10-06 10:29:03 -0600

    Added app.jsx and fixed typo in index.jsx

--- index.jsx
+++ index.jsx
@@ -3,4 +3,4 @@ import ReactDOM from 'react-dom/client';
 import App from './src/app';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
-root.reander(<App />);
+root.render(<App />);
