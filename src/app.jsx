52162b7689ba1946b4f5988d228a095c29b181dd
wjchorak <westonjchorak@gmail.com>
2025-10-07 15:47:12 -0600

    Navbar code v1 in app.jsx

--- src/app.jsx
+++ src/app.jsx
@@ -2,23 +2,47 @@ import React from 'react';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './app.css';
 
-export default function App() {
+import { BrowserRouter, NavLink, Route, Routes, useLocation } from 'react-router-dom';
+import { Login } from './login/login';
+import { Play } from './play/play';
+import { About } from './about/about';
+
+const NAVBAR_ROUTES = ['/play', '/about'];
+
+const Navbar = () => {
     return (
-        <div className="body">
-            <header id="page-header">
-                <nav class="navbar fixed-top navbar-dark">
-                    <h1 class="navbar-brand" href="#">Blackjack</h1>
-                </nav>
-            </header>
+        <menu className="navbar-nav">
+            <li className="nav-item"><NavLink className="nav-link" to="play">Play</NavLink></li>
+            <li className="nav-item"><NavLink className="nav-link active" to="about">About</NavLink></li>
+            <li className="nav-item"><NavLink className="nav-link" to="index">Logout</NavLink></li>
+        </menu>
+    );
+};
 
-            <main>App components go here</main>
+export default function App() {
+    const location = useLocation();
+    const showNavbar = NAVBAR_ROUTES.includes(location.pathname);
 
-            <footer>
-                <br />
-                <span>A site by Weston Chorak</span>
-                <br />
-                <a href="https://github.com/wjchorak/startup">Github</a>
-            </footer>
-        </div>
+    return (
+        <BrowserRouter>
+            <div className="body">
+                <header id="page-header">
+                    <nav className="navbar fixed-top navbar-dark">
+                        <h1 className="navbar-brand" href="#">Blackjack</h1>
+                    </nav>
+
+                    {showNavbar && <Navbar />}
+                </header>
+
+                <main>App components go here</main>
+
+                <footer>
+                    <br />
+                    <span>A site by Weston Chorak</span>
+                    <br />
+                    <a href="https://github.com/wjchorak/startup">Github</a>
+                </footer>
+            </div>
+        </BrowserRouter>
     );
 }
