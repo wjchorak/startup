60a3f3d280bfc6a7f1b4e36f0691b8f2c2d04e8b
wjchorak <westonjchorak@gmail.com>
2025-10-08 20:18:01 -0600

    CSS modules for login.jsx

--- src/login/login.jsx
+++ src/login/login.jsx
@@ -1,25 +1,25 @@
 import React from 'react';
-import './login.css';
+import styles from './login.module.css';
 
 export function Login() {
     return (
-        <main>
-            <div id="login-page">
+        <main className={styles.main}>
+            <div className={styles.loginPage}>
                 <h1>Login</h1>
                 <form method="get" action="play">
                     <div>
                         <span>Username</span>
-                        <input className="login-enter" type="text" />
+                        <input className={styles.loginEnter} type="text" />
                     </div>
                     <br />
                     <div>
                         <span>Password</span>
-                        <input className="login-enter" type="password" />
+                        <input className={styles.loginEnter} type="password" />
                     </div>
                     <br />
-                    <div id="login-controls">
-                        <button className="button-outline" type="submit">Login</button>
-                        <button className="button-outline" type="submit">Create</button>
+                    <div className={styles.loginControls}>
+                        <button className={`button-outline ${styles.buttonOutline}`} type="submit">Login</button>
+                        <button className={`button-outline ${styles.buttonOutline}`} type="submit">Create</button>
                     </div>
                 </form>
             </div>
