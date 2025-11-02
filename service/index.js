7ea2b89ba0aa6e43096f4094fdcabc1ff1859997
wjchorak <westonjchorak@gmail.com>
2025-11-01 19:16:23 -0600

    Transplanted code from Simon for login/scores basis

--- service/index.js
+++ service/index.js
@@ -7,7 +7,7 @@ const app = express();
 const authCookieName = 'token';
 
 let users = [];
-let credites = [];
+let credits = [];
 
 const port = process.argv.length > 2 ? process.argv[2] : 3000;
 
@@ -19,6 +19,126 @@ app.use(express.static('public'));
 
 var apiRouter = express.Router();
 app.use('/api', apiRouter);
+
+// THESE ENDPOINTS ARE ALL COPIED/ADAPTED FROM SIMON CODE
+
+// CreateAuth a new user
+apiRouter.post('/auth/create', async (req, res) => {
+    if (await findUser('userName', req.body.userName)) {
+        res.status(409).send({ msg: 'Existing user' });
+    } else {
+        const user = await createUser(req.body.userName, req.body.password);
+
+        setAuthCookie(res, user.token);
+        res.send({ userName: user.userName });
+    }
+});
+
+// GetAuth login an existing user
+apiRouter.post('/auth/login', async (req, res) => {
+    const user = await findUser('userName', req.body.userName);
+    if (user) {
+        if (await bcrypt.compare(req.body.password, user.password)) {
+            user.token = uuid.v4();
+            setAuthCookie(res, user.token);
+            res.send({ userName: user.userName });
+            return;
+        }
+    }
+    res.status(401).send({ msg: 'Unauthorized' });
+});
+
+// DeleteAuth logout a user
+apiRouter.delete('/auth/logout', async (req, res) => {
+    const user = await findUser('token', req.cookies[authCookieName]);
+    if (user) {
+        delete user.token;
+    }
+    res.clearCookie(authCookieName);
+    res.status(204).end();
+});
+
+// Middleware to verify that the user is authorized to call an endpoint
+const verifyAuth = async (req, res, next) => {
+    const user = await findUser('token', req.cookies[authCookieName]);
+    if (user) {
+        next();
+    } else {
+        res.status(401).send({ msg: 'Unauthorized' });
+    }
+};
+
+// GetCredits
+apiRouter.get('/credits', verifyAuth, (_req, res) => {
+    res.send(credits);
+});
+
+// SubmitScore
+apiRouter.post('/score', verifyAuth, (req, res) => {
+    credits = updateCredits(req.body);
+    res.send(credits);
+});
+
+// Default error handler
+app.use(function (err, req, res, next) {
+    res.status(500).send({ type: err.name, message: err.message });
+});
+
+// Return the application's default page if the path is unknown
+app.use((_req, res) => {
+    res.sendFile('index.html', { root: 'public' });
+});
+
+// updateCredits considers a new score for inclusion in the high credits/scores list.
+function updateCredits(newScore) {
+    let found = false;
+    for (const [i, prevScore] of credits.entries()) {
+        if (newScore.score > prevScore.score) {
+            credits.splice(i, 0, newScore);
+            found = true;
+            break;
+        }
+    }
+
+    if (!found) {
+        credits.push(newScore);
+    }
+
+    if (credits.length > 3) {
+        credits.length = 3;
+    }
+
+    return credits;
+}
+
+async function createUser(userName, password) {
+    const passwordHash = await bcrypt.hash(password, 10);
+
+    const user = {
+        userName: userName,
+        password: passwordHash,
+        token: uuid.v4(),
+    };
+    users.push(user);
+
+    return user;
+}
+
+async function findUser(field, value) {
+    if (!value) return null;
+
+    return users.find((u) => u[field] === value);
+}
+
+// setAuthCookie in the HTTP response
+function setAuthCookie(res, authToken) {
+    res.cookie(authCookieName, authToken, {
+        maxAge: 1000 * 60 * 60 * 24 * 365,
+        secure: true,
+        httpOnly: true,
+        sameSite: 'strict',
+    });
+}
 
 app.listen(port, () => {
     console.log(`Listening on port ${port}`);
