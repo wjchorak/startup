d82fd7e3589858bbeeb33753ac0704f208a9892c
wjchorak <westonjchorak@gmail.com>
2025-11-25 16:42:56 -0700

    Added backend peerProxy

---
+++ service/peerProxy.js
@@ -0,0 +1,32 @@
+import { WebSocketServer } from 'ws';
+
+function peerProxy(server) {
+    const socketServer = new WebSocketServer({ server: server });
+
+    socketServer.on('connection', (socket) => {
+        socket.isAlive = true;
+
+        socket.on('message', function message(data) {
+            socketServer.clients.forEach((client) => {
+                if(client !== socket && client.readyState === WebSocket.OPEN) {
+                    client.send(data);
+                }
+            });
+        });
+
+        socket.on('pong', () => {
+            socket.isAlive = true;
+        });
+    });
+
+    setInterval(() => {
+        socketServer.clients.forEach(function each(client) {
+            if (client.isAlive === false) return client.terminate();
+
+            client.isAlive = false;
+            client.ping();
+        });
+    }, 10000);
+}
+
+export default peerProxy;
